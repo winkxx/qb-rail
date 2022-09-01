02835a18864ef82bef13f9b1063dc55796d3e9b2
winkxx <68945988+winkxx@users.noreply.github.com>
2022-09-01 21:15:03 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#安装qb
+wget -qO /usr/bin/qbittorrent-nox https://github.com/userdocs/qbittorrent-nox-static/releases/latest/download/x86_64-qbittorrent-nox
+chmod 700 /usr/bin/qbittorrent-nox
+
+#安装rclone
+bash <(curl -sL install.emengweb.com/fclone.sh)
+mv /usr/bin/fclone /usr/bin/rclone
+chmod 777 /usr/bin/rclone
