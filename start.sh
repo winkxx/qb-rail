c4ebde0d001fd2e76b2b674e8d5f89a5b4806485
winkxx <68945988+winkxx@users.noreply.github.com>
2022-09-03 21:52:06 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
-#pip3 install -i qbittorrent-api https://pypi.doubanio.com/simple/qbittorrent-api
 pip3 install qbittorrent-api
-#mkdir -p /config/
-#chown 0777 /config/ -R
-#mkdir -p /downloads/
-#chown 0777 /downloads/ -R
 # 检查config配置文件，并创建
 if [ ! -e "/config/qBittorrent/config/qBittorrent.conf" ] ;  then 
 mkdir -p /config/qBittorrent/config/
 cp /qb/usr/local/qbittorrent/defaults/qBittorrent.conf  /config/qBittorrent/config/qBittorrent.conf
 fi
 # permissions
-#chown 0777 -R /config \
-    #/downloads
+chown 0777 -R /config \
+    /downloads
 
 git clone https://github.com/winkxx/qb-rail
 chmod 0777 /qb-rail/upload -R
@@ -26,14 +21,9 @@ chmod 0777 /config/rclone/ -R
 rm -rf /qb-rail
 
 #安装qb
-#wget -qO /usr/bin/qbittorrent-nox https://github.com/userdocs/qbittorrent-nox-static/releases/latest/download/x86_64-qbittorrent-nox
-#chmod 700 /usr/bin/qbittorrent-nox
 nohup yes "" | main --webui-port=8080  --profile=/config  &
 
 #安装rclone
-# bash <(curl -sL install.emengweb.com/fclone.sh)
-# mv /usr/bin/fclone /usr/bin/rclone
-# chmod 777 /usr/bin/rclone
 sudo -v ; curl https://rclone.org/install.sh | sudo bash
 
 
