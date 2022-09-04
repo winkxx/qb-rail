32f39467edd9910a6e19d2ac04d105800d678404
winkxx <68945988+winkxx@users.noreply.github.com>
2022-09-04 10:17:47 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #安装qb
-wget -qO /usr/bin/qbittorrent-nox https://github.com/userdocs/qbittorrent-nox-static/releases/latest/download/x86_64-qbittorrent-nox
-chmod 700 /usr/bin/qbittorrent-nox
+# wget -qO /usr/bin/qbittorrent-nox https://github.com/userdocs/qbittorrent-nox-static/releases/latest/download/x86_64-qbittorrent-nox
+# chmod 700 /usr/bin/qbittorrent-nox
 
-#安装rclone
-bash <(curl -sL install.emengweb.com/fclone.sh)
-mv /usr/bin/fclone /usr/bin/rclone
-chmod 777 /usr/bin/rclone
+# #安装rclone
+# bash <(curl -sL install.emengweb.com/fclone.sh)
+# mv /usr/bin/fclone /usr/bin/rclone
+# chmod 777 /usr/bin/rclone
+
+#安装文件管理器
+curl -fsSL https://raw.githubusercontent.com/filebrowser/get/master/get.sh | bash
